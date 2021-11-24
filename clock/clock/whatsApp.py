b76cfc70c38a7b9cbf68f6c8f7f90ce84fa81017
mhmad wrekat <mhmmadwreekkat@gmail.com>
2021-11-24 13:19:46 +0200

    add send whatsApp message

---
+++ clock/clock/whatsApp.py
@@ -0,0 +1,6 @@
+# pip install pywhatkit
+
+import pywhatkit
+
+pywhatkit.sendwhatmsg("+962798596928", "ارسال مسج بالعربيه باستخدام بايثون !!" , 13, 11, 30 , True, 60 )
+#pywhatkit.sendwhatmsg("+269 + Phone Number", "The Messsage" , Hour Time , Minit , Send After(in second), bool : close After , Close Time (Second))
